d7bfe8f0fdb84c807556ce0598721ecef88bac0f
Julija <juleczek2002@gmail.com>
2022-11-08 14:15:35 +0100

    Task9 done but this code is making me sad

--- WDAI/3_lab/zad9/script.js
+++ WDAI/3_lab/zad9/script.js
@@ -1,38 +1,90 @@
-var employees = 3;
-var data = [["first.jpeg", "Jan Kowalski", "Product Manager", "lorem"], ["second.webp", "Mariusz Pudzianowski", "Sportowiec", "ipsum"], ["third.jpeg", "Robert Makłowicz", "Kucharz i podróznik", "tralala"]];
-var idx = 0;
+let arr = Array.from(document.getElementsByClassName("middleMain"));
 
-function overwritten(idx){
-    let photo = document.getElementById("photo");
-    let name = document.querySelector("h2");
-    let title = document.querySelector("h3");
-    let description = document.querySelector("p");
+function randomCard(){
+    let min = 0;
+    let max = 2;
+    let chosen = Math.floor(Math.random() * (max - min + 1))
 
-    photo.src=data[idx][0];
-    name.innerText=data[idx][1];
-    title.innerText=data[idx][2];
+    let previousNumber = chosen - 1;
+    let nextNumber = chosen + 1;
+
+
+    if (chosen == 0){
+        previousNumber = 2;
+        nextNumber = 1;
+    }
+    else if (chosen == 2){
+        previousNumber = 1;
+        nextNumber = 0;
+    }
+
+
+    let previousElement = arr[previousNumber];
+    let nextElement = arr[nextNumber];
+    let newElement = arr[chosen];
+
+    previousElement.removeAttribute("class");
+    previousElement.style="";
+    previousElement.setAttribute("class", "middleMain");
+    previousElement.classList.add("leftMain")
+    nextElement.removeAttribute("class");
+    nextElement.style=""
+    nextElement.setAttribute("class", "middleMain");
+    nextElement.classList.add("rightMain");
+    newElement.removeAttribute("class");
+    newElement.style="";
+    newElement.setAttribute("class", "middleMain");
+
+    //zmieniam kolejność w tablicy, aby potem dobrze działało przy przesuwaniu w lewo bądź prawo
+    arr = [previousElement, newElement, nextElement];
 }
 
-function rightClick(){
-    idx += 1;
-    if (idx == employees){
-        idx = 0;
-    }
-    console.log(idx);
 
-    overwritten(idx);
-    
+
+
+function rightClick(){
+
+    let hiding = arr[1]; //chowa się środkowy
+    let newSlide = arr[0]; //wjeżdża ten po lewej
+    let rest = arr[2];
+
+    hiding.style.transform = "translate(135%)";
+    hiding.style.transition = "1s ease";
+
+    newSlide.style.transform = "translate(0%)";
+    newSlide.style.transition = "1s ease";
+
+    rest.style.transform = "translate(-132%)"
+    rest.style.transition = "0s"
+
+
+    //teraz podmianka kolejności w tablicy arr
+    arr = [rest, newSlide, hiding];
+    return arr;
 }
 
 function leftClick(){
-    idx -= 1;
-    if (idx == -1){
-        idx = 2;
-    }
 
-    overwritten(idx);
+    let hiding = arr[1]; //chowa się środkowy
+    let newSlide = arr[2]; //wjeżdża ten po prawej
+    let rest = arr[0];
+    
+    hiding.style.transform = "translate(-135%)";
+    hiding.style.transition = "1s ease";
+
+    newSlide.style.transform = "translate(0%)";
+    newSlide.style.transition = "1s ease";
+
+    rest.style.transform = "translate(132%)"
+    rest.style.transition = "0s"
+
+
+    //teraz podmianka kolejności w tablicy arr
+    arr = [ hiding, newSlide, rest];
+    return arr;
 }
 
 
 document.getElementById("right").addEventListener("click", rightClick);
 document.getElementById("left").addEventListener("click", leftClick);
+document.getElementById("lotto").addEventListener("click", randomCard);
